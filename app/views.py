6990a2b11585efcd1ca2966e532835f13ad940be
Daniel T. Lee <danieltimlee@gmail.com>
2019-02-18 17:31:53 +0900

    Server: Simple server sending command to CLI
    Signed-off-by: Daniel T. Lee <danieltimlee@gmail.com>

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
 from app import app
 from flask import request, jsonify
+import subprocess
 
 @app.route("/", methods=["GET"])
 def index():
     return jsonify(message="Hello World!"), 200
+
+
+@app.route("/cmd", methods=["POST"])
+def cmd():
+    req = request.get_json()
+    command = 'sudo ' + req['command']
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    output, err = p.communicate()
+
+    res = output.decode("utf-8")
+    return jsonify(result=res), 200
