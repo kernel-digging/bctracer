bf11df2c391bc52c30dd93c00abd870e140891e4
Daniel T. Lee <danieltimlee@gmail.com>
2019-02-19 20:58:41 +0900

    Server: Code-Fetch API implemented
    Signed-off-by: Daniel T. Lee <danieltimlee@gmail.com>

--- app/views.py
+++ app/views.py
@@ -16,3 +16,17 @@ def cmd():
 
     res = output.decode("utf-8")
     return jsonify(result=res), 200
+
+# TODO: validate json object
+@app.route("/api/code", methods=["POST"])
+def code():
+    req = request.get_json()
+    cmd = "grep -A 35 -Fx \"$(awk 'NR=={line}' {path}/{file})\" {path}/{file}".format(**req, path="/home/danieltl/git/linux")
+    command = 'sudo ' + cmd
+
+    print(command)
+
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    output, err = p.communicate()
+    res = output.decode("utf-8")
+    return jsonify(result=res), 200
